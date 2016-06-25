083220b29c573cf5526fd86e928c9399fc7dbf79
Sushil <sushil@knoldus.com>
2016-06-25 07:51:12 +0530

    Added UI Added webjar dependency for ui-jquery Added search template

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,23 @@
 package controllers
 
+import javax.inject.Inject
+
+import play.api.libs.json.Json
 import play.api.mvc._
 import views.html
 
-class Application extends Controller {
+class Application @Inject()(webJarAssets: WebJarAssets) extends Controller {
 
-  def index = Action {
-    Ok(html.index("play-elasticsearch-autocomplate"))
+  def index = Action {Ok(html.index(webJarAssets))}
+
+  def searchText(term: String) = Action {
+    val searchList = {
+      if (term == "j")
+        List("Java", "Javascript", "Jquery")
+      else
+        List("Levis", "Levis men shirt")
+    }
+    Ok(Json.toJson(searchList))
   }
 
 }
--- build.sbt
+++ build.sbt
@@ -8,5 +8,11 @@ scalaVersion := "2.11.8"
 
 libraryDependencies ++= Seq(
   "org.elasticsearch" % "elasticsearch" % "2.3.3",
+
+  // WebJars (i.e. client-side) dependencies
+  "org.webjars" %% "webjars-play" % "2.5.0",
+  "org.webjars" % "jquery" % "1.11.3",
+  "org.webjars" % "bootstrap" % "3.3.6" exclude("org.webjars", "jquery"),
+  "org.webjars" % "jquery-ui" % "1.9.2" exclude("org.webjars", "jquery"),
     specs2 % Test
 )
